74804b9329d35fd05d22888f8306984ae1d11c7d
Netanel Tal Nizri <netanel0058@gmail.com>
2020-02-25 00:32:37 +0200

    Add BuildAgentManager.bat

---
+++ BuildAgentManager.bat
@@ -0,0 +1,30 @@
+@echo off
+title Build Agent Manager
+echo Build Agent Manager
+echo =============================================
+echo.
+
+net session > nul 2>&1
+IF NOT %errorLevel% == 0 (
+    echo ---- Hint: `Run as administrator` ----
+)
+
+choice /C ED /N /M "(E)nable / (D)isable:"
+IF ERRORLEVEL 2 GOTO disable
+IF ERRORLEVEL 1 GOTO enable
+
+:enable
+echo.
+net start TeamCity > nul 2> nul
+net start TCBuildAgent > nul 2> nul
+echo Done!
+pause > nul
+exit
+
+:disable
+echo.
+net stop TeamCity > nul 2> nul
+net stop TCBuildAgent > nul 2> nul
+echo Done!
+pause > nul
+exit
